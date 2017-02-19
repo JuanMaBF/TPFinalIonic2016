f126416b7a9cfb0e6a0216e0f28182320e756c02
Juan Manuel Barreda Fafasuli <juanmanuelbf@gmail.com>
2017-02-19 15:41:40 -0300

    commit

--- TPdef/www/js/controllers.js
+++ TPdef/www/js/controllers.js
@@ -6,6 +6,10 @@ angular.module('app.controllers', [])
    
 .controller('usuariosOnlineCtrl', function ($scope, $stateParams) {
 
+  /*var ref = new Firebase("https://votacion-f0c45.firebaseio.com/UsuariosOnline");
+  ref.on("value", function(snapshot){
+
+  });*/
 
 })
    
@@ -16,7 +20,6 @@ angular.module('app.controllers', [])
    
 .controller('crearDesafioCtrl', function ($scope, $stateParams) {
 
-
 })
    
 .controller('desafioCreadoCtrl', function ($scope, $stateParams) {
@@ -26,60 +29,60 @@ angular.module('app.controllers', [])
    
 .controller('desafioAceptadoCtrl', function ($scope, $stateParams) {
 
-
 })
       
-.controller('loginCtrl', function ($scope, $stateParams, $ionicPopup, $timeout) {
+.controller('loginCtrl', function ($scope, $stateParams, $ionicPopup, $timeout, $firebaseArray) {
 
-    $scope.loginData = {};
+  $scope.loginData = {};
 	$scope.login = function(){
-	    /*Validar datos de logueo*/
-	    if($scope.loginData.email == null){ 
-	      alert("Ingrese un email");
-	      return -1;
-	    }else if($scope.loginData.password == null){
-	      alert("Ingrese un password");
-	      return -1;
-	    }
-	    /*Crear variables con datos de logueo*/
-	    var email = $scope.loginData.email;
-	    var password = $scope.loginData.password;
+	  /*Validar datos de logueo*/
+	  if($scope.loginData.email == null){ 
+	    alert("Ingrese un email");
+	    return -1;
+	  }else if($scope.loginData.password == null){
+	    alert("Ingrese un password");
+	    return -1;
+	  }
+	  /*Crear variables con datos de logueo*/
+	  var email = $scope.loginData.email;
+	  var password = $scope.loginData.password;
 
-	    /*Loguear con mail y password*/
-	    firebase.auth().signInWithEmailAndPassword(email, password)
-	      .then(function(usuario){ /*Logueo exitoso*/
-	        firebase.User = usuario;
-	        location.href="#/inicio/usuariosOnline"; //Redireccionamiento
-	        var myPopup = $ionicPopup.show({
-	           template: '<center> Bienvenido ' + firebase.auth().currentUser.displayName + "</center>",
-	           title: 'Bienvenido'
-	        });
-	        $timeout(function(){
-	          myPopup.close();
-
-	        }, 3000);
-	        
-	      })
-	      .catch(function(error) { /*Manejo de errores*/
-	        var errorCode = error.code;
-	        var errorMessage = error.message;
-	        if (errorCode == 'auth/invalid-email') {
-	          alert('El email ingresado no es válido.');
-	        } 
-	        else if (errorCode == 'auth/user-disabled') {
-	          alert('El usuario ha sido deshabilitado.');
-	        } 
-	        else if (errorCode == 'auth/user-not-found') {
-	          alert('El usuario ingresado no existe.');
-	        } 
-	        else if (errorCode == 'auth/wrong-password') {
-	          alert('El password ingresado es incorrecto.');
-	        } 
-	        else {
-	          alert(errorMessage); //Otro tipo de error
-	        }
-	        console.info(error);
-		});
+	  /*Loguear con mail y password*/
+	  firebase.auth().signInWithEmailAndPassword(email, password)
+	    .then(function(usuario){ /*Logueo exitoso*/
+	      firebase.User = usuario;
+        firebase.database().ref('UsuariosOnline/').push({
+          email: email
+        });          
+	      location.href="#/inicio/usuariosOnline"; //Redireccionamiento
+	      var myPopup = $ionicPopup.show({
+	        template: '<center> Bienvenido ' + firebase.auth().currentUser.displayName + "</center>",
+	        title: 'Bienvenido'
+	      });
+	      $timeout(function(){
+	        myPopup.close();
+	      }, 3000);  
+	    })
+	    .catch(function(error) { /*Manejo de errores*/
+	      var errorCode = error.code;
+	      var errorMessage = error.message;
+	      if (errorCode == 'auth/invalid-email') {
+	        alert('El email ingresado no es válido.');
+	      } 
+	      else if (errorCode == 'auth/user-disabled') {
+	        alert('El usuario ha sido deshabilitado.');
+	      } 
+	      else if (errorCode == 'auth/user-not-found') {
+	        alert('El usuario ingresado no existe.');
+	      } 
+	      else if (errorCode == 'auth/wrong-password') {
+	        alert('El password ingresado es incorrecto.');
+	      } 
+	      else {
+	        alert(errorMessage); //Otro tipo de error
+	      }
+	      console.info(error);
+		  });
 	}
 
 })
@@ -89,7 +92,6 @@ angular.module('app.controllers', [])
   $scope.regData = {};
   // Hacer logueo
   $scope.registrar = function() {
-
     /*Validar datos de logueo*/
     if($scope.regData.nombre == null){ 
       alert("Ingrese un nombre.");
@@ -117,6 +119,10 @@ angular.module('app.controllers', [])
           var displayName = usuario.displayName;
         }, function(error) {
           console.info(error);
+        });
+
+        firebase.database().ref('UsuariosOnline/').push({
+          email: email
         });
 
         var myPopup = $ionicPopup.show({
